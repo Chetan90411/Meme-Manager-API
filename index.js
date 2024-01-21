b4d948a07ee49e6ec7aec8d56ea28eab3bf5819d
Chetan90411 <chetan738090@gmail.com>
2024-01-21 20:31:35 +0530

    front end setup

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ connectDB();
 app.use(json());
 app.use(userRouter);
 app.use(memeRouter);
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.use(express.static('./public'))
 
 app.listen(port, () => {
   console.log("Server started at port", port);
