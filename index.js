132b5b094721eb2d0791310b07b6856d0827f2b1
Chetan90411 <chetan738090@gmail.com>
2024-01-21 21:20:08 +0530

    login page added

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ import memeRouter from "./src/router/meme.js";
 const app = express();
 const port = process.env.PORT;
 
+
 connectDB();
 
 app.use(json());
 app.use(userRouter);
 app.use(memeRouter);
+
+app.set("view engine", "ejs");
+app.set('views', "./public/html")
 app.use(express.static('./public'))
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
 
 app.listen(port, () => {
   console.log("Server started at port", port);
